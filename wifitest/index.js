feac34d961f4877fd10704e855e386f8c180daaf
Atsing Hou <houaq@qq.com>
2017-12-19 16:49:27 +0800

    staging

--- wifitest/index.js
+++ wifitest/index.js
@@ -16,14 +16,17 @@ var table = {};
 
 mc.on('message', function (topic, message) {
 	var line = message.toString();
+	line = line.split('$')[0];
   var record = line.split(', ');
 	var ip = record[0];
 	var row = table[ip];
 	if (!row) {
 		var i = Object.keys(table).length + 1;
-		table[ip] = row = { i: i, age: 0 };
-	} else
+		table[ip] = row = { i: i, age: 0, lost: 0 };
+	} else {
 		row.age = 0;
+		row.lost = 0;
+	}
 
 	cur.movePos(row.i, 1);
 	console.log('%s %s', ' '.repeat(10), line);
@@ -33,10 +36,14 @@ setInterval(function () {
 	// aging record
 	for (var ip in table) {
 		var row = table[ip];
-		if (row.age < cols) {
+		if (row.age < 10) {
 			row.age++;
 			cur.movePos(row.i, 1);
 			console.log('>'.repeat(row.age));
+		} else {
+			row.lost = 1;
+			cur.movePos(row.i, 1);
+			console.log('[31m-- LOST --[m');
 		}
 	}
 }, 1000);
