a796293f040b75627452ce1cfae800c2c2822b48
Atsing Hou <houaq@qq.com>
2019-05-20 14:21:52 +0800

    megatec UPS simulator

---
+++ simulators/megatec.lua
@@ -0,0 +1,44 @@
+﻿#!/usr/bin/env lua
+package.path = './?.lua'.. package.path
+
+local tty = require('tty')
+
+local port = arg[1] or '/dev/ttyS0'
+
+local responses = {
+	I = "#Kehua_Tech_Ltd. 20KVA      v1.1_2013 \r",
+	F = "#320.0 050 02.13 50.0\r",
+	Q1 = {
+		"(208.4 140.0 208.4 034 59.9 2.05 35.0 00000000\r",
+		"(208.4 140.0 208.4 094 59.9 1.65 55.0 00010000\r",
+		"(208.4 140.0 208.4 034 59.9 1.65 55.0 00010000\r",
+		"(000.0 140.0 208.4 034 59.9 2.05 35.0 10010000\r"
+	},
+	G1 = "!240 094 0123 025.0 +35.0 50.1 52.0 50.0\r",
+	G2 = "!00000010 00000100 00000000\r",
+	G3 = "!222.0/222.0/222.0 221.0/221.0/221.0 220.0/220.0/220.0 014.0/015.0/014.0\r"
+}
+
+local portfd, err = tty.open(port, 2400, 0)
+if not portfd then
+    print('failed to open port:'.. err)
+    os.exit(1)
+end
+
+while true do
+	local ok, line = tty.readline(portfd, '\r')
+	if not ok then
+		print('readline not ok, line is: '.. line)
+	else
+		print('Got command: '.. line)
+		line = string.gsub(line, '\r', '')
+		local resp = responses[line]
+		if resp then
+			if type(resp) == 'table' then
+				resp = resp[math.random(4)]
+			end
+			print('Responding with: '.. resp)
+			tty.write(portfd, resp)
+		end
+	end
+end
---
+++ simulators/tty.lua
@@ -0,0 +1,75 @@
+--
+-- tty.lua: Linux serial port IO (based on posix.termio)
+--
+-- Copyright(C) 2019, HouAQ
+--
+local U = require('posix.unistd')
+
+local M = {}
+
+--- open serial port
+-- @port the serial port dev
+-- @baud baudrate, default: 9600
+-- @timeout read timeout in seconds, default: 1s
+-- @parity default: no parity
+--
+M.open = function(port, baud, timeout, parity)
+    local T = require('posix.termio')
+    local F = require('posix.fcntl')
+    local fd, err = F.open(port, F.O_RDWR + F.O_NOCTTY + F.O_EXCL)
+    if not fd then
+        return nil, err
+    end
+
+    baud = baud or 9600
+    timeout = timeout or 1
+    local vmin = 0
+    if timeout == 0 then vmin = 1 end
+
+    local cflag = T.CS8 + T.CLOCAL + T.CREAD
+    if parity then
+        cflag = cflag + T.PARENB
+        if parity == 'odd' then
+            cflag = cflag + T.PARODD
+        end
+    end
+
+    T.tcsetattr(
+        fd, 0,
+        {
+            cflag = cflag,
+            iflag = T.IGNPAR,
+            ispeed = T['B' .. baud],
+            ospeed = T['B' .. baud],
+            cc = {
+                [T.VTIME] = timeout * 10,
+                [T.VMIN] = vmin
+            }
+        }
+    )
+
+    return fd
+end
+
+M.write = function(fd, data)
+    return U.write(fd, data)
+end
+
+M.readline = function(fd, eol)
+    local line = ''
+    eol = eol or '\n'
+    while true do
+        local c = U.read(fd, 1)
+        if not c or c == '' then
+            return false, line
+        end
+
+        line = line .. c
+
+        if c == eol then
+            return true, line
+        end
+    end
+end
+
+return M
